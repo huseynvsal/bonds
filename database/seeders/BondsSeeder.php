c566f6d789a41b0159a6ae81ed8a5c48434d6a7b
huseynvsal <huseynlivusal25@gmail.com>
2022-06-09 21:54:26 +0400

    Fixed seeders

--- database/seeders/BondsSeeder.php
+++ database/seeders/BondsSeeder.php
@@ -20,8 +20,8 @@ class BondsSeeder extends Seeder
                 'issue_date'=>'2021-11-08',
                 'last_turnover_date'=> '2022-11-03',
                 'nominal_price'=> 100,
-                'payment_frequency'=> 4,
-                'calculating_period'=> 360,
+                'payment_frequency'=> '4',
+                'calculating_period'=> '360',
                 'coupon_interest'=> 10,
                 'created_at'=>date('Y-m-d H:i:s'),
                 'updated_at'=>date('Y-m-d H:i:s')
@@ -30,8 +30,8 @@ class BondsSeeder extends Seeder
                 'issue_date'=>'2021-11-08',
                 'last_turnover_date'=> '2022-11-07',
                 'nominal_price'=> 200,
-                'payment_frequency'=> 4,
-                'calculating_period'=> 364,
+                'payment_frequency'=> '4',
+                'calculating_period'=> '364',
                 'coupon_interest'=> 20,
                 'created_at'=>date('Y-m-d H:i:s'),
                 'updated_at'=>date('Y-m-d H:i:s')
@@ -40,8 +40,8 @@ class BondsSeeder extends Seeder
                 'issue_date'=>'2021-11-08',
                 'last_turnover_date'=> '2022-11-08',
                 'nominal_price'=> 300,
-                'payment_frequency'=> 4,
-                'calculating_period'=> 365,
+                'payment_frequency'=> '4',
+                'calculating_period'=> '365',
                 'coupon_interest'=> 30,
                 'created_at'=>date('Y-m-d H:i:s'),
                 'updated_at'=>date('Y-m-d H:i:s')
--- database/seeders/OrdersSeeder.php
+++ database/seeders/OrdersSeeder.php
@@ -19,17 +19,23 @@ class OrdersSeeder extends Seeder
             [
                 'bond_id'=> 1,
                 'order_date'=> '2021-11-23',
-                'bonds_quantity'=> 10
+                'bonds_quantity'=> 10,
+                'created_at'=>date('Y-m-d H:i:s'),
+                'updated_at'=>date('Y-m-d H:i:s')
             ],
             [
                 'bond_id'=> 2,
                 'order_date'=> '2021-11-23',
-                'bonds_quantity'=> 10
+                'bonds_quantity'=> 10,
+                'created_at'=>date('Y-m-d H:i:s'),
+                'updated_at'=>date('Y-m-d H:i:s')
             ],
             [
                 'bond_id'=> 3,
                 'order_date'=> '2021-11-23',
-                'bonds_quantity'=> 10
+                'bonds_quantity'=> 10,
+                'created_at'=>date('Y-m-d H:i:s'),
+                'updated_at'=>date('Y-m-d H:i:s')
             ]
         ];
 
